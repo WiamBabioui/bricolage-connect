d9c07a1cb6d3b1b4eccedc24bce672d591889c07
Wiam_BABIOUI <wiam.babioui@teknologiate.com>
2026-04-05 21:48:32 +0100

    Fix: ajout du Port et du SSL pour la connexion Aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,16 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    // ⚠️ AJOUTE LE PORT (Indispensable pour Aiven)
+    port: process.env.DB_PORT || 3306, 
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    // ⚠️ AJOUTE LE SSL (Obligatoire pour Aiven en ligne)
+    // On l'active seulement si on n'est pas sur ton PC (localhost)
+    ssl: process.env.DB_HOST !== 'localhost' ? {
+        rejectUnauthorized: false
+    } : null
 });
 
 module.exports = pool;
